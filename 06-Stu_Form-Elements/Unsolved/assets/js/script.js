f0e9f0a9e915208df5a63726eb8649b7d2837b86
Old Prego <spymasterluke@gmail.com>
2021-03-03 20:17:03 -0600

    updated exercise 6 with more simplistic selectors on the shopping input

--- 06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,6 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
-var shoppingInput = $('input[name="shopping-input"]');
+var shoppingInput = $('#shopping-input');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
 
